3aacafa3ef9a9eba03da1c3db9a241e272e0fff0
datawiz168 <140295050+datawiz168@users.noreply.github.com>
2023-08-13 00:14:42 +0800

    Add files via upload

---
+++ k8s搭建.py
@@ -0,0 +1,83 @@
+'''python
+Kubernetes集群搭建指南：
+
+### 1. 环境准备
+#### a. 操作系统
+- 使用Ubuntu 18.04或更高版本。
+- 更新所有包：`sudo apt-get update && sudo apt-get upgrade`。
+
+#### b. 硬件要求
+- Master节点：2核CPU、2GB RAM、30GB硬盘。
+- Worker节点：1核CPU、2GB RAM、30GB硬盘。
+
+#### c. 网络配置
+- 主机名唯一。
+- 主机间互通。
+- 能访问Internet。
+
+### 2. 安装Docker
+```bash
+sudo apt update
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker --version
+```
+
+### 3. 安装kubeadm、kubelet和kubectl
+```bash
+sudo apt-get update
+sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo swapoff -a
+```
+
+### 4. 初始化Master节点
+```bash
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+```
+
+### 5. 部署Pod网络
+```bash
+kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+```
+
+### 6. 加入Worker节点
+```bash
+sudo kubeadm join <master-ip>:6443 --token <token> --discovery-token-ca-cert-hash <hash>
+```
+
+### 7. 验证集群
+```bash
+kubectl get nodes
+kubectl create deployment nginx --image=nginx
+kubectl expose deployment nginx --port=80 --type=LoadBalancer
+```
+
+### 8. 高级配置和优化
+- 设置资源限制。
+- 配置存储。
+- 配置日志和监控。
+- 高可用性配置。
+
+### 9. 日常维护
+- 更新和升级。
+- 备份和恢复。
+
+### 10. 结论
+Kubernetes集群的部署和管理是一个复杂的过程，涉及许多配置和优化。查阅[Kubernetes官方文档](https://kubernetes.io/docs/)可获得更多信息。
+
+请注意，此指南主要用于学习和实验。生产环境可能需要更多安全、高可用性、灾难恢复等方面的考虑。如果您有更多具体问题或需要深入解释，请随时提问！
+
+
+
+'''
